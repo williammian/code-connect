0aaf71d55808927e284752ab134a681369c0e5ee
William Mian <william_mian@yahoo.com.br>
2024-10-21 14:49:45 -0300

    paginas dinamicas

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,30 +1,32 @@
 import Image from "next/image"
 import { Avatar } from "../Avatar"
 import styles from './cardpost.module.css'
+import Link from "next/link"
 
-export const CardPost = ({ post }) => {
+export const CardPost = ({ post, highlight }) => {
     return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure>
-                    <Image 
-                        src={post.cover} 
-                        width={438} 
-                        height={133} 
-                        alt={`Capa do post de titulo: ${post.title}`} 
+        <Link href={`/posts/${post.slug}`} className={styles.link}>
+             <article className={styles.card} style={{ width: highlight ? 993 : 486}}>
+                <header className={styles.header}>
+                    <figure style={{ height: highlight ? 300 : 133}}>
+                        <Image
+                            src={post.cover}
+                            fill
+                            alt={`Capa do post de titulo: ${post.title}`}
+                        />
+                    </figure>
+                </header>
+                <section className={styles.body}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </section>
+                <footer className={styles.footer}>
+                    <Avatar
+                        imageSrc={post.author.avatar}
+                        name={post.author.username}
                     />
-                </figure>
-            </header>
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-            </section>
-            <footer className={styles.footer}>
-                <Avatar 
-                    imageSrc={post.author.avatar} 
-                    name={post.author.username}
-                />
-            </footer>
-        </article>
+                </footer>
+            </article>
+        </Link>
     )
 }
